94cec8420f1507653eb20799f7bbb8f61ac75d4d
Syahroni Wahyu <roniwahyu@gmail.com>
2024-05-08 16:27:24 +0700

    Delete siakadrps2 directory

--- siakadrps2/resources/lang/pt/pagination.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'next'     => 'Próxima &raquo;',
-    'previous' => '&laquo; Anterior',
-];
--- siakadrps2/resources/views/components/sub-page.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="ajax-inline-page" data-url="{{ $url }}" data-props="{{ json_encode($props) }}">
-	
-</div>
