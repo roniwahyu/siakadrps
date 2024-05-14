18fc57e2cdb69618365816d4a013cdb0abdee693
roniwahyu <roniwahyu@gmail.com>
2024-05-14 10:23:17 +0700

    update id-ID

--- config/locales.php
+++ config/locales.php
@@ -1,14 +1,7 @@
 
 <?php 
 	return [
-		'fr' => 'French',
-'ru' => 'Russian',
-'zh-CN' => 'Chinese',
-'en-US' => 'English',
-'it' => 'Italian',
-'hi' => 'Hindi',
-'pt' => 'Portuguese',
-'de' => 'German',
-'es' => 'Spanish',
-'ar' => 'Arabic'
+		'id-ID' => 'Bahasa Indonesia',
+		'en-US' => 'English',
+
 	];
